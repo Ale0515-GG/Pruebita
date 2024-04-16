8e19b62f2bc4855050eb38878ad30f9d06736714
Alejandra Galvan <116208731+Ale0515-GG@users.noreply.github.com>
2024-04-16 14:40:08 -0600

    jj

--- server/routes/Publicaciones.js
+++ server/routes/Publicaciones.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', publicacionControllers.getPublicaciones);
 router.get('/:id', publicacionControllers.getPublicacionById);
-router.get('/autor/:autor', publicacionControllers.getPublicacionesByAutor); // Ruta para buscar publicaciones por autor
+router.get('/autor/:autor', publicacionControllers.getPublicacionesByAutor);
 router.post('/', publicacionControllers.createPublicacion);
 router.put('/:id', publicacionControllers.updatePublicacion);
 router.delete('/:id', publicacionControllers.deletePublicacion);
